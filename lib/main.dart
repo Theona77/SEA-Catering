f63d34d41b972be7d2a5c5e93e0ee839f11864f6
Theona77 <145539364+Theona77@users.noreply.github.com>
2025-06-27 22:55:14 +0700

    Icon category added and fixed some bugs

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:sea_catering/data/repositories.authentication/authentication_repository.dart';
 import 'package:sea_catering/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -30,6 +32,33 @@ Future<void>main() async {
     url: 'https://qzujmlvhdupzbltmgjyp.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6dWptbHZoZHVwemJsdG1nanlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4NzIzNzAsImV4cCI6MjA2NjQ0ODM3MH0.flzzVTSAU4Xjs6sTovVgef-NXlGGAJbIa7p5C569LrU',
   );
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Catch all Flutter framework errors
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.presentError(details);
+    // Optional: Log it or send to crashlytics
+  };
+
+  // Catch all Dart async errors
+  PlatformDispatcher.instance.onError = (error, stack) {
+    // Optional: Log error
+    return true; // prevents red screen
+  };
+
+
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    return Container(
+      color: Colors.white,
+      child: const Center(
+        child: Text(
+          'Something went wrong',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  };
 
 
   runApp(const MyApp());
